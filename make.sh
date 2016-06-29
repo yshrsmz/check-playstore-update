311646cfbad4b1d72fbae2c36fa04a29d02da029
yshrsmz <the.phantom.bane@gmail.com>
2016-06-29 17:32:51 +0900

    add binaries

--- make.sh
+++ make.sh
@@ -3,13 +3,13 @@
 # go get github.com/roylee0704/gron
 # go get gopkg.in/yaml.v2
 #
-# for GOOS in darwin linux; do
-#     export GOOS=$GOOS
-#     for GOARCH in 386 amd64; do
-#         export GOARCH=$GOARCH
-#         go build -v -o bin/check-playstore-update-$GOOS-$GOARCH
-#     done
-# done
-
-export GOARCH=386
-go build -v -o bin/check-playstore-update-linux-386
+for GOOS in darwin linux; do
+    export GOOS=$GOOS
+    for GOARCH in 386 amd64; do
+        export GOARCH=$GOARCH
+        go build -v -o bin/check-playstore-update-$GOOS-$GOARCH
+    done
+done
+#
+# export GOARCH=386
+# go build -v -o bin/check-playstore-update-linux-386
